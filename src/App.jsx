6d1a71b48dffe46d6adc4ad8c3b83d3e3090ba84
elicastropa <elizabethcastropa@gmail.com>
2024-05-14 17:43:42 -0500

    Panel de administracion

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RandomProductsList from './components/Card/RandomProductsList';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar.jsx';
+import Admin from './components/PanelAdministrador/Admin.jsx'
 import DetailProduct from './components/DetailProduct/DetailProduct.jsx';
 import FeaturedProducts from './components/ProductsFav/FeaturedProducts.jsx';
 import LogoFestivall from '../public/FestivallSVG.svg';
 import ProductCard from './components/Card/ProductCard.jsx';
 import CategorySection from './components/Categorias/CategorySection.jsx';
+import { MdAdminPanelSettings } from 'react-icons/md';
+import RegistrarProducto from './components/PanelAdministrador/RegistrarProducto.jsx';
 
 const menuItems = ['Nosotros', 'Servicios', 'Contacto', 'Galería'];
 
@@ -29,6 +32,8 @@ const App = () => {
           />
           <Route path='/detalle/:id' element={<DetailProduct />} />
           <Route path='/product/:id' element={<ProductCard />} />
+          <Route path='/RegistrarProducto' element={<RegistrarProducto />} />
+          <Route path='/admin' element={<Admin />} />
         </Routes>
         <Footer />
       </BrowserRouter>
