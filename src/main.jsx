f32cb57ef54dc31a06a9e05c7ab96973097c9aa6
Lenin Uzcategui <leuzga@gmail.com>
2024-05-24 17:59:11 -0400

    Feature/pig5 c login and toasty components (#23)
    * Agregando la pantalla del login, notificador
    
    * Agregando gitignore modificado
    
    ---------
    
    Co-authored-by: Lenin Uzcategui <c-luzcategu@cdsreg.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+
+// import { Auth0Provider } from '@auth0/auth0-react';
+
 import '@fontsource/playfair-display';
 import '@fontsource/playfair-display/400.css';
 import '@fontsource/playfair-display/400-italic.css';
@@ -10,8 +13,10 @@ import './index.css';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Auth0Provider> */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
