1c3dd244b05bfe9500960d1040abb2e0019f5335
Lenin Uzcategui <c-luzcategu@cdsreg.com>
2024-05-23 20:03:47 -0400

    Agregando la pantalla del login, notificador

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
-// import { Auth0Provider } from '@auth0/auth0-react';
-
+import { Auth0Provider } from '@auth0/auth0-react';
 import '@fontsource/playfair-display';
 import '@fontsource/playfair-display/400.css';
 import '@fontsource/playfair-display/400-italic.css';
@@ -13,10 +11,10 @@ import './index.css';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Auth0Provider> */}
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-    {/* </Auth0Provider> */}
+    <Auth0Provider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
