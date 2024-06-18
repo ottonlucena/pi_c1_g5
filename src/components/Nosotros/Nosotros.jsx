a690281c593a211f921706afcbd5239f7ed280db
elicastropa <elizabethcastropa@gmail.com>
2024-06-18 10:56:26 -0500

    nosotros responsive

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -5,27 +5,27 @@ import Styles from './Nosotros.module.css';
 const teamMembers = [
     {
         name: 'Elizabeth Castro',
-        role: 'CEO',
+        role: 'Scrum Master',
         image: '../../../public/assets/Elizabeth.jpg'
     },
     {
         name: 'Lenin Uzcategui',
-        role: 'CTO',
+        role: 'Team Leader',
         image: '../../../public/assets/Lenin.jpg'
     },
     {
         name: 'Juan Manuel Herrero',
-        role: 'Desarrollador',
+        role: 'Desarrollador Senior',
         image: '../../../public/assets/juanma.jpg'
     },
     {
         name: 'Otton Lucena',
-        role: 'Diseñadora',
+        role: 'Database Expert',
         image: '../../../public/assets/Otton.jpg'
     },
     {
         name: 'Jorge Alfaro',
-        role: 'Marketing',
+        role: 'Back-end /Infra',
         image: '../../../public/assets/jorge.jpg'
     },
     {
@@ -38,10 +38,20 @@ const teamMembers = [
 const Nosotros = () => {
     return (
         <div className={Styles.nosotros}>
+            <div className={Styles.container1}>
             <h1>Somos un gran equipo</h1>
-            <div className="team-container">
+            <p>Nos complace presentarles al equipo detrás de nuestra exitosa empresa de alquiler de juegos de feria.
+
+Somos un grupo de profesionales apasionados por crear experiencias inolvidables para todos nuestros clientes. Cada miembro de nuestro equipo aporta una combinación única de creatividad, entusiasmo y experiencia en la organización de eventos. Nos encanta ver a la gente disfrutar y divertirse con nuestros juegos y atracciones.
+
+Desde la instalación hasta la operación y el desmontaje, nuestro equipo se asegura de que cada detalle esté perfectamente cuidado. Estamos comprometidos con la seguridad, la calidad y la satisfacción del cliente. Ya sea una fiesta de cumpleaños, una feria escolar, o un evento corporativo, nuestro objetivo es hacer de su celebración un momento especial y memorable.
+
+Gracias por elegirnos para sus eventos. ¡Estamos emocionados de ser parte de sus momentos más felices y entretenidos!</p>
+
+            </div>
+            <div className={Styles.teamContainer}>
                 {teamMembers.map((member, index) => (
-                    <div key={index} className="team-member">
+                    <div key={index} className={Styles.teamMember}>
                         <img src={member.image} alt={member.name} className={Styles.memberPhoto} />
                         <h2>{member.name}</h2>
                         <p>{member.role}</p>
